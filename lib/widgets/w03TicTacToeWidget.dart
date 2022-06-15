4361c16aa325fcdb24197fc01e46695241e251ed
jonathan-ulises <jonulisanpar@gmail.com>
2022-06-15 03:06:16 -0500

    nuevo widget

---
+++ lib/widgets/w03TicTacToeWidget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TicTacToeWidget extends StatefulWidget {
+  @override
+  _TicTacToeWidgetState createState() => _TicTacToeWidgetState();
+}
+
+class _TicTacToeWidgetState extends State<TicTacToeWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TicTacToe'),
+      ),
+      body: Center(),
+    );
+  }
+}
